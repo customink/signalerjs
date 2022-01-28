13e5f784b50a264a69c0a109888d709c9a228ec5
Kate Piette <kat.piette@gmail.com>
2022-01-28 15:11:56 -0500

    Updated helpers.test.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,10 @@
 export function defaultDomain(hostname) {
-  var hostArray = hostname.split('.');
+  const hostArray = hostname.split('.');
   return hostArray.length > 2 ? `.${hostArray.slice(1).join('.')}` : `.${hostname}`;
 }
 
 export function daysAfterToday(days) {
-  var currentDate = Date.now();
-  var msInDay = 24 * 3600 * 1000;
+  const currentDate = Date.now();
+  const msInDay = 24 * 3600 * 1000;
   return (new Date(currentDate + days * msInDay)).toUTCString();
 }
